fa27c05216b7c939f0a6c34490d244c88dc11d9d
Elvis Mbugua <manfuturekahande@gmail.com>
2025-01-30 21:18:53 +0300

    Movie Details done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MovieDisplay from './components/MovieDisplay';
 import MovieDetails from './components/MovieDetails';
 import './index.css';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const getMovies = async () => {
-      const movies = await fetchMovies();
-      setMovies(movies);
-    };
-
-    getMovies();
-  }, []);
-
   return (
     <Router>
       <div className="App">
-        <h1>Popular Movies</h1>
+        <h1>Movie App</h1>
         <Routes>
-          <Route path="/" element={<MovieDisplay movies={movies} />} />
+          <Route path="/" element={<MovieDisplay />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
         </Routes>
       </div>
