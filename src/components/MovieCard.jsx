bfb49a83ce6b57b83a0ee3b7f26ba6d0dbb2608b
felix mwangi <fmwauramwangi@gmail.com>
2025-02-03 08:50:52 +0300

    src/styles/MovieCard.css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/MovieCard.css';
 
-const MovieCard = ({ movie }) => {
-//function to handle adding the movie to the watch list
+const MovieCard = ({ movie }) => {//function to handle adding the movie to the watch list
   const handleWatchList = () => {
     console.log(`"${movie.title}" added to watch list`);
 //log the movie title to the console
@@ -15,6 +14,7 @@ const MovieCard = ({ movie }) => {
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}//movie poster path from the api
         alt={movie.title}/>
         <h4>{movie.title}</h4>
+        
         <button onClick={handleWatchList}//triger handleWatchlist when clicked
         className='function-button watchList'>
           Add to watch list
